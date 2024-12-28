e729e38a2bce6a463310490e4e8f9de049311bad
Joaquin <joaqobianchi13@gmail.com>
2024-12-28 19:16:26 -0300

    Cambio de convencion de _id por id

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,23 +1,33 @@
-import { Schema, model } from 'mongoose';
-import { ITask } from '../types/task';
+import { Schema, model } from "mongoose";
+import { ITask } from "../types/task";
 
-const taskSchema = new Schema<ITask>({
-  title: {
-    type: String,
-    required: [true, 'Title is required']
+const taskSchema = new Schema<ITask>(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  description: {
-    type: String,
-    required: false
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
+  {
+    toJSON: {
+      transform: (_, ret) => {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
   }
-});
+);
 
-export const Task = model<ITask>('Task', taskSchema);
+export const Task = model<ITask>("Task", taskSchema);
